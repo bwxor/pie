ef91a156a1d6aa80fb6ee9f51129f7123375b601
mateasmario <mmmateas@gmail.com>
2025-10-23 19:19:06 +0300

    fix typo

--- plugin/Classes/Actions/ShowNotificationAction.cs
+++ plugin/Classes/Actions/ShowNotificationAction.cs
@@ -1,6 +1,6 @@
 ﻿/* SPDX-FileCopyrightText: 2023-2025 Mario-Mihai Mateas <mateasmario@aol.com> */
 /* SPDX-License-Identifier: GPL-3.0-or-later */
-4
+
     namespace plugin.Classes.Actions
 {
     public class ShowNotificationAction : UIAction
